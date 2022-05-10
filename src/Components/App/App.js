4b45e21731b9898c5b75e8142edaec7ef019f95d
Matt Parisien <matthewparisien4@gmail.com>
2022-05-10 18:05:08 -0400

    Remove scroll wrapper - no longer using loco scroll

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,10 @@
-import { createContext, useEffect, useRef, useState } from "react";
+import { createContext, useRef, useState } from "react";
 import Header from "../../Header/Header";
-import Compose from "../Compose/Compose";
 import EntryScreen from "../EntryScreen/EntryScreen";
 import Footer from "../Footer/Footer";
 import Hero from "../Hero/Hero";
-import Results from "../Results/Results";
-import ScrollTop from "../ScrollTop/ScrollTop";
-import Section from "../Section/Section";
 import MainInterface from "../MainInterface/MainInterface";
-import Cursor from "../Cursor";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-import { useLocomotiveScroll } from "react-locomotive-scroll";
+import ScrollTop from "../ScrollTop/ScrollTop";
 import "./App.css";
 
 export const GlobalContext = createContext();
