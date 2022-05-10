978e5714905767ce56335a0f17331299d6b9c9a3
Matt Parisien <matthewparisien4@gmail.com>
2022-05-10 17:05:06 -0400

    Fix hero spacing

---
+++ src/Components/Vector/Star.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Star() {
+	return (
+		<div className='Star  absolute top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 z-50'>
+			<svg
+				className='Star_svg w-full h-full'
+				xmlns='http://www.w3.org/2000/svg'
+				viewBox='0 0 398.89 407.59'
+			>
+				<path
+					className='cls-1'
+					d='M223.11,539.31,388.89,167'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M149.89,484.13l312.22-262'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M108.26,402.44l395.48-98.6'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M106.66,310.77l398.68,84.74'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M145.41,227.67,466.59,478.61'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M216.66,170,395.34,536.31'
+					transform='translate(-106.56 -149.34)'
+				/>
+				<path
+					className='cls-1'
+					d='M306,149.34V556.93'
+					transform='translate(-106.56 -149.34)'
+				/>
+			</svg>
+		</div>
+	);
+}
+
+export default Star;
