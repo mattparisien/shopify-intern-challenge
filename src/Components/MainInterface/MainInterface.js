df8a425bdbbce30b48df36024271cedafa81e574
Matt Parisien <matthewparisien4@gmail.com>
2022-05-11 18:05:32 -0400

    Break flex layout on mobile (MainInterface)

--- src/Components/MainInterface/MainInterface.js
+++ src/Components/MainInterface/MainInterface.js
@@ -8,12 +8,12 @@ function MainInterface() {
 	return (
 		<>
 			<Section name='MainInterface'>
-				<div className='content-wrapper flex w-full py-40'>
-					<div className='flex flex-col w-2/4 pr-10'>
+				<div className='content-wrapper flex flex-col md:flex-row w-full py-20 md:y-40'>
+					<div className='flex flex-col mb-20 w-full md:w-2/4 md:pr-10 md:mb-0'>
 						<Compose />
 						<Search />
 					</div>
-					<div className="w-2/4 pl-10">
+					<div className="w-full md:w-2/4 md:pl-10">
 						<Results />
 					</div>
 				</div>
