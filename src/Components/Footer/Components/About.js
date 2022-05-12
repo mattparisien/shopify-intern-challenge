37ad2fa2f3a55e7f93e246d82aa418a10df5886b
Matt Parisien <matthewparisien4@gmail.com>
2022-05-12 16:31:33 -0400

    Fix header height

--- src/Components/Footer/Components/About.js
+++ src/Components/Footer/Components/About.js
@@ -3,13 +3,17 @@ import Heading from "../../Heading/Heading";
 
 function About() {
 	return (
-		<div className='Footer_About flex items-start justify-between py-40'>
-			<Heading>About</Heading>
-			<p className='text-3xl w-2/3 text-left text-cream uppercase'>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore
-				incidunt consequuntur aliquam iure? Placeat, amet quos. Pariatur
-				quibusdam cupiditate veritatis, quasi repellendus omnis nostrum ullam.
-				Debitis sapiente impedit.
+		<div className='Footer_About flex items-start justify-between pb-40 pt-10 border-t relative'>
+			<div className='absolute text-cream top-0 left-0 text-xs pt-3'>
+				{" "}
+				● About
+			</div>
+
+			<p className='text-4xl md:text-5xl indent-60 w-full md:w-3/4 text-left text-cream uppercase'>
+				PROMPTIT IS A TINY FRONT-END → APPLICATION BUILT WITH REACT AND
+				TAILWINDCSS. THE APP ALLOWS USERS TO ENTER PROMPTS AND RECEIVE
+				DIRECT RESPONSES FROM OPENAI'S API ●. NOT FOR PROFESSIONAL USE.
+				
 			</p>
 		</div>
 	);
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,7 @@ import Container from "../Container/Container";
 import Search from "../Search/Search";
 import "./Header.css";
 import SearchButton from "./SearchButton";
+import useScroll from "../../hooks/useScroll";
 
 function Header() {
 	const [searchActive, setSearchActive] = useState(false);
@@ -13,14 +14,14 @@ function Header() {
 
 	return (
 		<header
-			className={`Header fixed top-0 left-0 w-screen h-20 bg-transparent text-cream z-50`}
+			className={`Header fixed top-0 left-0 w-screen h-12 bg-dark text-cream z-50`}
 		>
 			<Container>
 				<div className='content flex w-full h-full items-center justify-between'>
-					<div className='header-logo transition ease duration-300 text-2xl'>
-						Prompt it!
+					<div className='header-logo transition ease duration-300 text-lg text-left flex-1'>
+						Pr●mpt it!
 					</div>
-					<div className='header-controls w-full flex items-center justify-end'>
+					<div className='header-controls flex-1 flex items-center justify-end'>
 						<Search isActive={searchActive} />
 						<SearchButton onClick={onClick} isSearchActive={searchActive} />
 					</div>
