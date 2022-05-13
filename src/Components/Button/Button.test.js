f0e05568dd933f812e8c518e0cf4dd3e073e5df1
Matt Parisien <matthewparisien4@gmail.com>
2022-05-13 12:50:10 -0400

    Implement basic unit tests for button component

---
+++ src/Components/Button/Button.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import Button from "./Button";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+	render(<Button />);
+});
+
+it("renders a clickable button", () => {
+	const handleClick = jest.fn();
+
+	const { getByText } = render(
+		<Button onClick={handleClick}>Clickable</Button>
+	);
+	const button = getByText("Clickable").closest("button");
+
+	fireEvent.click(button);
+	expect(handleClick).toHaveBeenCalledTimes(1);
+});
+
+it("renders a button label", () => {
+	const { getByText } = render(<Button>Default</Button>);
+	expect(getByText("Default")).toBeInTheDocument();
+});
+
+it("renders an outline button style", () => {
+	const { getByText } = render(<Button variant={"outline"}>Default</Button>);
+	expect(getByText("Default").closest("button")).toHaveClass("Button_outline");
+});
